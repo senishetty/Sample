bc67b324b6162507eaf17d80edc106c59be8e12a
Sushma Enishetty <senishetty@primesoft.local>
2016-03-24 14:48:14 +0530

    Add my project

---
+++ Simple/src/test/java/testing/SampleTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public static void test1()
+	{
+		WebDriver driver =new FirefoxDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+		driver.quit();
+	}
+
+}
